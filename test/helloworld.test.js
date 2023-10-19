ad3c0096a8784de2e7451adf0086b8c0d8a229b1
Glen Sadler <sadsster@gmail.com>
2023-10-19 14:35:04 +1300

    Added files for automatic unit tests

---
+++ test/helloworld.test.js
@@ -0,0 +1,8 @@
+import sayHello from '../utils/helloworld.js';
+import assert from 'assert';
+
+it('Should say hello', () => {
+    const hello = sayHello();
+
+    assert.equal(hello, 'hello');
+});
